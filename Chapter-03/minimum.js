aa0c3e070f91f93292064bc9c55dd14824e7efca
john <john@spinningplates.org>
2019-03-18 16:43:45 -0700

    added solution for minimum.js

---
+++ Chapter-03/minimum.js
@@ -0,0 +1,11 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(num1, num2) {
+  if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+console.log(min(0, 10));
+console.log(min(0, -10));
