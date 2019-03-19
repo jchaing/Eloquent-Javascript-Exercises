46f79b6885e522800f290bcad6292d8290644862
john <john@spinningplates.org>
2019-03-19 10:07:16 -0700

    added ternary operator to minimum.js

--- Chapter-03/minimum.js
+++ Chapter-03/minimum.js
@@ -1,5 +1,6 @@
 // Write a function min that takes two arguments and returns their minimum.
 
+// if statement
 function min(num1, num2) {
   if (num1 < num2) {
     return num1;
@@ -9,3 +10,11 @@ function min(num1, num2) {
 }
 console.log(min(0, 10));
 console.log(min(0, -10));
+
+// ternary operator
+function min(num1, num2) {
+  return num1 < num2 ? num1 : num2;
+}
+console.log(min(0, 10));
+console.log(min(0, -10));
+console.log(min(-1, -2));
