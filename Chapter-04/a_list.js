c436679f3077ab2fdef9207522a953c3fe8be84c
john <john@spinningplates.org>
2019-04-09 12:16:20 -0700

    updated a_list.js

--- Chapter-04/a_list.js
+++ Chapter-04/a_list.js
@@ -40,4 +40,4 @@ function prepend(element, list) {
   return newList;
 }
 
-console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null))); // {value: 10, rest: {value: 20, rest: null}}
